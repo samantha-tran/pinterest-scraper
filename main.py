bd02f534b9862efc20b4b8c6ee11d65018c1fef5
samantha-tran <stran322001@gmail.com>
2023-08-18 20:54:19 +1000

    added stubs

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import argparse
 from typing import List
 
-def scrape_board(api:str, url:List[str], collage:bool):
-
-    # get scraper type
+def scrape_image_urls(api:str, url:List[str]):
 
     # scrape
 
     # process
     pass
 
+def create_collage():
+    pass
 
 
+def save_images():
+    pass
 
-def main():
+def save_to_location():
+    pass
+
+
+def parse_arguments():
     # top level parser
     parser = argparse.ArgumentParser(description='Processes pinterest images')
     parser.add_argument('url', type=str, action='append', help='Pinterest Board URL')  # can scrape multiple boards
@@ -25,15 +31,33 @@ def main():
     subparsers.add_parser('save', help='Save pins as images in a folder')
 
     collage_subparser = subparsers.add_parser('collage', help='Create collage of pins')
-    collage_subparser.add_argument('minHeight', metavar='MinimumImageHeight',
-                                   type=int, help='Minimum width of an image in pixels')
-    collage_subparser.add_argument('collageWidth', metavar='CollageWidth',
-                                   type=int, help='Width of the collage in pixels')
-    collage_subparser.add_argument('collageHeight', metavar='CollageHeight',
-                                   type=int, help='Height of collage in pixels')
+    collage_subparser.add_argument('minHeight', type=int, help='Minimum width of an image in pixels')
+    collage_subparser.add_argument('collageWidth', type=int, help='Width of the collage in pixels')
+    collage_subparser.add_argument('collageHeight', type=int, help='Height of collage in pixels')
+
+    return parser.parse_args()
+
+def main():
+    # parse aguments
+    args = parse_arguments()
+
+    # scrape images
+    imageURLs = scrape_image_urls()
+
+    # transform images
+    if args.collage:
+        result = create_collage()
+    elif args.save:
+        result = save_images()
+    else:
+        pass
+
+    save_to_location(result)
 
 
-    args = parser.parse_args()
+
+    # save result
+
 
 if __name__ == "__main__":
     main()
